0fcb83ca59c81867ab48c7615a80986050776b46
Kenny Whyte <kawhyte@gmail.com>
2020-11-10 16:50:42 -0800

    Init

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,13 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
 
         <p className={styles.description}>
           Get started by editing{' '}
